e14dcf690a28909b27f9ca6c440730cf74187b91
yoko <yoko.hashimoto.29@gmail.com>
2019-03-09 13:55:32 +0900

    devise導入、nameカラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'artworks#index'
   resources :artworks
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
