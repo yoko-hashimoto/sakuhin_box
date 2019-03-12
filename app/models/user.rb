32711f0983e2cd5a396362c5da6b4a623f36bc6a
yoko <yoko.hashimoto.29@gmail.com>
2019-03-13 06:33:42 +0900

    deviseno日本語対応を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   
   # 1人のユーザーは複数のクリエイターを保有している
   has_many :creators
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+# デフォルトの言語設定を日本語に設定
+I18n.config.available_locales = :ja
+I18n.default_locale = :ja
